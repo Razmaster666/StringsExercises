c87aaa25bb4fc0cf647288a30159ab5d0101fb91
RasmusClausen <rasmusclausen123@gmail.com>
2022-11-12 19:43:29 +0100

    aadsdasd

--- src/Task4.java
+++ src/Task4.java
@@ -1,2 +1,21 @@
 public class Task4 {
+
+//    Task 4 – Split and trim
+//    Use this String instead and create a solution with the same output as in task 3:
+//    String names = "Apple, Banana, Lemon";
+//    Hint:
+//    Use the trim method on the strings to remove the space character in the beginning of the second
+//    and third string.
+
+    public static void main(String[] args) {
+
+        String names = "Apple, Banana, Lemon";
+
+        String[] namesSplit = names.split(",");
+
+        for(String i : namesSplit){
+            System.out.println("Remember to buy: " + (i.trim()) + "!");
+        }
+
+    }
 }
